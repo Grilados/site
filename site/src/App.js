516baef53c98551974330d6cc22db52d075cfc8d
VitorSuperlogica <vitor.alves.superlogica@gmail.com>
2017-08-27 14:31:20 -0300

    .

---
+++ site/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ site/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './css/Header.css';
+import {Navbar} from 'react-bootstrap';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <Navbar>
+                Isso é o Header
+            </Navbar>
+        )
+    }
+}
